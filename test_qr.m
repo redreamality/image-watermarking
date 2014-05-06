b03c04fef5ac169f7fc7d7ed59de70318a2ce6fe
redreamality <redreamality@gmail.com>
2014-05-06 13:42:14 +0300

    dct_test

--- test_qr.m
+++ test_qr.m
@@ -7,9 +7,10 @@ javaaddpath('.\zxing-1.7-core.jar');
 javaaddpath('.\zxing-1.7-javase.jar');
 
 % encode a new QR code
-test_encode = encode_qr('la la la', [32 32]);
-figure;imagesc(test_encode);axis image;
+% test_encode = encode_qr('la la la', [32 32]);
+% figure;imagesc(test_encode);axis image;
 
 % decode
-pic = imread('test_qr.jpg')
+% pic = imread('test_qr.jpg');
+pic = imread('Snapshot_20140326.jpg');
 message = decode_qr(pic)
